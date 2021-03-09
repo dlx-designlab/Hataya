4e61c78d214af09bf5de0cd3978593454d313a89
yurikleb <yurikleb@gmail.com>
2021-03-10 02:26:32 +0900

    takover logic quick test

--- main.py
+++ main.py
@@ -1,44 +1,86 @@
-import threading
-from gpiozero import RotaryEncoder, Servo
 from time import sleep
+import threading
+from gpiozero import RotaryEncoder, AngularServo
 
-SERVO = Servo(22)
-ROTOR = RotaryEncoder(17, 18, max_steps=0)
+# use custom pin-factory to fix servo jitter, make sure pigpio deamon is running: 'sudo pigpiod'
+from gpiozero.pins.pigpio import PiGPIOFactory
+pigpio_factory = PiGPIOFactory()
 
 
-SERVO.mid()
-sleep(3)
-ROTOR.steps = 0
+ENCODER = RotaryEncoder(17, 18, max_steps=0)
+SERVO = AngularServo(22, min_angle=-90, max_angle=90, pin_factory=pigpio_factory)
+ENC_SRV_RATIO = 2 # How many encoder steps is one servo angle
+TAKEOVER_OFFSET = 40 #The difference (in angles) between the Encoder and the Servo posiotn to trigger manual override
+IS_AV_MODE = True
+
+
+SERVO.angle = 0
+print("Servo set to mid. Move hande to center position.")
+sleep(5)
+
+print("Resetting encoder")
+ENCODER.steps = 0
 
 
 def encoder_reader():
-  global ROTOR
+  global ENCODER
   encoder_done = threading.Event()  
   print('Turn the encoder!!')
-  
-  ROTOR.when_rotated = update_rotor
+
+  ENCODER.when_rotated = get_encoder_step
   encoder_done.wait()
 
-def update_rotor():
-    global ROTOR
-    steps = ROTOR.steps
-    print(steps)
+def get_encoder_step():
+    global ENCODER, SERVO, IS_AV_MODE, ENC_SRV_RATIO, TAKEOVER_OFFSET
+    enc_pos = ENCODER.steps
+
+    if IS_AV_MODE:
+      srv_pos = SERVO.angle
+      offset = (srv_pos * ENC_SRV_RATIO) - enc_pos
+      print(offset)
+      if abs(offset) > TAKEOVER_OFFSET:
+        IS_AV_MODE = False
+    else:
+      print(enc_pos)
 
 def servo_ctrl():
-  global SERVO
+  global SERVO, IS_AV_MODE, ENCODER, ENC_SRV_RATIO
   # servo_done = threading.Event()
-  print("Servo Online!")
-  while True:
-    SERVO.min()
-    print("servo pos min")
-    sleep(5)
-    SERVO.mid()
-    print("servo pos mid")
-    sleep(5)
-    SERVO.max()
-    print("servo pos max")
-    sleep(5)
+  delay = 0.1     #seconds
+  servo_step = 2  #angle
 
+  SERVO.max()
+  print("Moving to Max position")
+  sleep(2)
+  max_encoder = ENCODER.steps
+
+  print("Servo Online!")
+  SERVO.min()
+  print("Moving to Min position")
+  sleep(2)
+  min_encoder = ENCODER.steps
+
+  ENC_SRV_RATIO = (max_encoder - min_encoder) / (SERVO.max_angle - SERVO.min_angle)
+  print(f"Servo rng: {SERVO.min_angle}-{SERVO.max_angle} // Encoder rng: {min_encoder}-{max_encoder}")
+  print(f"Ratio: {ENC_SRV_RATIO}")
+  
+  SERVO.mid()
+  sleep(2)
+  IS_AV_MODE = True
+  while IS_AV_MODE == True:
+    
+    if SERVO.angle < (SERVO.min_angle - servo_step):
+      servo_step = 2
+      sleep(2)      
+    elif SERVO.angle > (SERVO.max_angle - servo_step):
+      servo_step = -2
+      sleep(2)
+    
+    SERVO.angle += servo_step
+    # print(SERVO.angle)
+    sleep(delay)
+
+# Start Threads!
 encoder_thread = threading.Thread(target=encoder_reader)
 encoder_thread.start()
 
@@ -47,4 +89,3 @@ servo_thread.start()
 
 # run endless thread
 # done.wait()
-
--- servo.py
+++ servo.py
@@ -1,12 +1,33 @@
-from gpiozero import Servo
+# use this custom pin-factory to fix servo jitter. 
+# make sure pigpio deamon is running: 'sudo pigpiod'
+from gpiozero.pins.pigpio import PiGPIOFactory
+
+from gpiozero import AngularServo
 from time import sleep
 
-servo = Servo(17)
+# create a custom pin-factory to fix servo jitter
+# more info heere: https://gpiozero.readthedocs.io/en/stable/api_output.html#servo
+# and here: https://gpiozero.readthedocs.io/en/stable/api_pins.html
+pigpio_factory = PiGPIOFactory()
+
+servo = AngularServo(22, min_angle=-90, max_angle=90, pin_factory=pigpio_factory)
+servo.angle = 0
+print("servo mid")
+sleep(3)
+
 
 while True:
-  # servo.min()
-  # sleep(1)
-  servo.mid()
-  sleep(5)
-  # servo.max()
-  # sleep(1)
+
+  delay = 0.1     #seconds
+  servo_step = 2  #angle
+
+  for i in range (servo.min_angle, servo.max_angle, 2):
+    servo.angle = i
+    print(f"servo pos {i}")
+    sleep(delay)
+
+
+  for i in range (servo.max_angle, servo.min_angle, -2):
+    servo.angle = i
+    print(f"servo pos {i}")
+    sleep(delay)
